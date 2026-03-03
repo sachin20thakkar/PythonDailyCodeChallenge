64093111bae65d14ae5541ace601795869f89d5e
Sachin Thakkar <sachin20thakkar@gmail.com>
2026-03-03 18:24:43 +0530

    02/02 DayPrediction

---
+++ feb/groundhogDayPrediction.py
@@ -0,0 +1,12 @@
+def groundhog_day_prediction(appearance):
+    if appearance is True:
+        return "Looks like we'll have six more weeks of winter."
+    elif appearance is False:
+        return "It's going to be an early spring."
+    else:
+        return "No prediction this year."
+
+# Test cases
+print(groundhog_day_prediction(True))   # "Looks like we'll have six more weeks of winter."
+print(groundhog_day_prediction(False))  # "It's going to be an early spring."   
+print(groundhog_day_prediction(None))   # "No prediction this year."
